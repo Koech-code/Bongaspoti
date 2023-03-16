9cde85cb58d89fc9f76aba7d08eaf2f6da957e38
Koech-code <nixonkipkorir01@gmail.com>
2023-03-16 09:03:10 +0300

    created a a homepage that displays feeds posted by users

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,39 +1,87 @@
 import React, { useState, useEffect } from 'react';
+import {HeartIcon, ChatIcon, PlusIcon, ShareIcon, PhotographIcon } from '@heroicons/react/outline';
+import {Video} from '@styled-icons/entypo/Video'
+import GetPosts from './GetPosts';
 
+import axios from 'axios';
 
 const HomePage = () => {
-   const [events, setEvents] = useState([]);
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState(null);
+  const [video, setVideo] = useState(null);  
+
+  function handleImageSelection(event) {
+    setImage(event.target.files[0]);
+  }  
+
+  function handleVideoSelection(evt){
+    setVideo(evt.target.files[0])
+  }
+   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('content', content);
+    formData.append('image', image);
+    formData.append('video', video);
  
-   useEffect(() => {
-     fetch('http://127.0.0.1:8000/api/events/all_events/')
-       .then((response) => response.json())
-       .then((data) => {
-         console.log(data);
-         setEvents(data);
-       })
-       .catch((err) => {
-         console.log(err.message);
-       });
-   }, []);
- 
+    
+    try {
+      await axios.post('http://127.0.0.1:8000/api/feed/create/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Token ${localStorage.getItem('token')}`,
+        },
+      });
+
+      alert('Post created successfully!');
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create post.');
+    }
+  };
+
    return (
-      
-         <div className="flex flex-wrap">
-      
-      {events.map((item) => (
-        <div key={item.id} className="w-full md:w-1/2 lg:w-1/3 p-4">
-          <div className="bg-white rounded-lg shadow-lg">
-            <div className="p-4">
-              <h2 className="text-lg font-medium mb-2">{item.title}</h2>
-              <img src={`http://127.0.0.1:8000${item.image}`} alt="event" />
-              <p className="text-gray-700">{item.description}</p>
-            </div>
+    
+     <div id="container">
+       <div className="home">
+        <form onSubmit={handleSubmit}>
+        <div className="bg-white shadow-lg rounded-lg p-6 m-4">
+        <input
+          className="border border-gray-300 rounded-md px-4 py-2 mb-4 w-full focus:outline-none focus:border-blue-500"
+          type="text"
+          onChange={(evt) => setContent(evt.target.value)}
+          placeholder="Lets know what you need to share with the world..."
+        />
+        <div className="flex justify-between">
+          <div className="flex">
+          <div className="relative rounded-full h-8 w-8">
+              <PhotographIcon className="absolute inset-0 h-full w-full text-blue-600" />
+              <input
+                type="file"
+                onChange={handleImageSelection}
+                className="absolute inset-0 h-full w-full opacity-0 cursor-pointer"
+              />
           </div>
+          <div className="relative rounded-full h-8 w-8">
+             <Video className="absolute inset-0 h-full w-full text-blue-600" />
+             <input
+               type="file"
+               onChange={handleVideoSelection}
+               className="absolute inset-0 h-full w-full opacity-0 cursor-pointer"
+             />
+          </div>
+          </div>
+          <button type='submit' className="p-2 text-sm bg-blue-600 hover:bg-white-700 text-white font-bold py-2 px-4 rounded">
+            Tweet
+          </button>
         </div>
-      ))}
-        
-    </div>
-     
+           </div>
+           </form>
+         </div>
+         <GetPosts/>
+     </div>    
    );
  };
  
