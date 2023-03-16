f599fede6dedc1c8b416123eec60e025d2a784f2
Koech-code <nixonkipkorir01@gmail.com>
2023-03-16 09:00:29 +0300

    create a modal pop up on clicking comment icon

---
+++ src/components/Modal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const PopupCard = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={togglePopup}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Open Popup
+      </button>
+
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
+
+          <div className="bg-white rounded-lg p-8 z-10">
+            <h2 className="text-2xl font-bold mb-4">Popup Title</h2>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button
+              onClick={togglePopup}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Close Popup
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PopupCard;
