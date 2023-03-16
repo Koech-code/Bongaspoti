690c37a39d29a63b96a65a7c69cf5ea3891048f6
Koech-code <nixonkipkorir01@gmail.com>
2023-03-16 09:02:22 +0300

    created a login page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
@@ -13,14 +17,20 @@ function Login() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch('http://127.0.0.1:8000/api/login/', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    // Handle the response from Django here
+    const url = 'http://127.0.0.1:8000/api/login/';
+    const data = { username: username, password: password };
+    try {
+      const response = await axios.post(url, data);
+      localStorage.setItem('token', response.data.token);
+      console.log(response.data);
+      // Redirect to homepage on success
+      window.location.href = '/feed';
+      alert('Login successful');
+    } 
+    catch (error) {
+      console.error(error);
+      alert('Failed to Login');
+    }
   };
 
   const handleChange = (event) => {
@@ -32,39 +42,35 @@ function Login() {
   };
 
 return (
-    <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
-      <div className="mb-4">
-        <label htmlFor="username" className="block font-medium mb-2">
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto shadow-lg rounded-lg mt-4 pl-4 pr-4 pb-4 pt-4">
+      <div className="mb-4 mt-10">
+        <label htmlFor="username" className="block font-medium mb-2  ">
           Username
         </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleInputChange}
+        <input 
+          type="text" 
+          value={username} 
+          onChange={(event) => setUsername(event.target.value)} 
+          className='w-full border-gray-400 border-solid border-2 rounded-md px-3 py-2 focus:outline-none focus:border-green-500'
           required
-          className="w-full border-gray-400 border-solid border-2 rounded-md px-3 py-2"
         />
       </div>
    
       <div className="mb-4">
-        <label htmlFor="password" className="block font-medium mb-2">
+        <label htmlFor="password" className="block font-medium mb-2 ">
           Password
         </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(event) => setPassword(event.target.value)} 
+          className='w-full border-gray-400 border-solid border-2 rounded-md px-3 py-2 center focus:outline-none focus:border-green-500'
           required
-          className="w-full border-gray-400 border-solid border-2 rounded-md px-3 py-2"
         />
       </div>
       <button
         type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded"
       >
         Login
       </button>
