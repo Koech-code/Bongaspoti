4998c6a60f34047d6145ed7730164550bf5e92d4
Koech-code <nixonkipkorir01@gmail.com>
2023-03-31 16:26:41 +0300

    added a know athlete page

--- src/components/KnowAthlete.js
+++ src/components/KnowAthlete.js
@@ -4,12 +4,11 @@ const KnowAthlete = () => {
   return (
     <div>
        <p className='bg-[#22354D] md:px-8 lg:px-24 sm:text-1xl text-white text-4xl md:text-5xl font-bold text-center md:mt-2 mb-8 md:mb-12 p-24'>Get to know our athletes</p> 
-       <h1 className='text-center'>Select an athlete</h1> 
-       <p className='text-center'>
-            Awesome Name provides its services to athletes of track and field events. 
-            To find out about our available athletes please select the event  
-            you are interested in below.
-        </p>
+       <div className="bg-white rounded-lg shadow-sm p-4 lg:w-1/3 w-1/3 m-8">
+    <h2 className="text-lg font-bold mb-2 text-center text-blue-600">1. Assessment</h2>
+    <p className="text-gray-600 p-4 lg:p-8">Our team consist of athletes and former athletes who evaluate
+      your submitted registration requirements and if successful move you to the next step.</p>
+  </div>
     </div>
   )
 }
